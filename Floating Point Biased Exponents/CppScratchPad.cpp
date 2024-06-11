b05a19c4162ce814c7eb892bca0e320c93c4e60f
Kulpot <kulpotwahu@gmail.com>
2024-06-12 00:41:28 +0800

    //Floating Point Biased Exponents   //ref link:https://www.youtube.com/watch?v=0zrUGfLg7u8&list=PLRwVmtr-pp04ljVWgUFlQoBl9tqTzFh2N&index=4

--- Floating Point Biased Exponents/CppScratchPad.cpp
+++ Floating Point Biased Exponents/CppScratchPad.cpp
@@ -2,6 +2,8 @@
 using std::cout;
 using std::endl;
 
+//Floating Point Biased Exponents   //ref link:https://www.youtube.com/watch?v=0zrUGfLg7u8&list=PLRwVmtr-pp04ljVWgUFlQoBl9tqTzFh2N&index=4
+
 void main()
 {
 	// 1.01101011100  000
